b5444b339de761c3ee23430fa3ce8213437b2dd8
hugh <indexhui@gmail.com>
2021-09-11 00:30:38 +0800

    admin router setting

---
+++ src/components/NavBarAdmin.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav>
+      <router-link to="/admin/tweets">推文清單</router-link>
+      <router-link to="/admin/users">使用者列表 </router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  display:flex;
+  flex-direction: column;
+  width: 235px;
+  > a {
+    display: block;
+    text-align: left;
+  }
+}
+</style>
