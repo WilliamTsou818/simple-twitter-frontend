317e7dd5bd7cd4ea96637d7a1b351b18b783a55d
YPINPIN <yping611@gmail.com>
2021-09-11 19:42:15 +0800

    modify to use axios interceptors

--- src/apis/users.js
+++ src/apis/users.js
@@ -4,8 +4,6 @@ const getToken = () => localStorage.getItem('token')
 export default {
   getCurrentUser() {
     // FIXME:餐廳測試用API
-    return apiHelper.get(`/get_current_user`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/get_current_user`)
   },
 }
