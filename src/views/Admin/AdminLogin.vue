ff377254da1f831e573d28caf407aa9c715bb0b0
YPINPIN <yping611@gmail.com>
2021-09-12 23:39:13 +0800

    modify to use LoginForm component

--- src/views/Admin/AdminLogin.vue
+++ src/views/Admin/AdminLogin.vue
@@ -3,94 +3,28 @@
     <div class="logo">
       <img src="@/assets/images/logo.svg" alt="logo" />
     </div>
-    <form class="form" @submit.prevent.stop="handSubmit">
-      <div class="form__title">後台登入</div>
-      <div class="form__email">
-        <input
-          class="form__email__input"
-          v-model="email"
-          id="email"
-          name="email"
-          type="email"
-          required
-        />
-        <label for="form__email__input" class="form__email__label">帳號</label>
-      </div>
-      <div class="form__password">
-        <input
-          class="form__password__input"
-          v-model="password"
-          id="password"
-          name="password"
-          type="password"
-          required
-        />
-        <label for="form__password__input" class="form__password__label"
-          >密碼</label
-        >
-      </div>
-      <button class="form__submit" type="submit" :disabled="isProcessing">
-        登入
-      </button>
-    </form>
+    <LoginForm :title="title" :path="path" />
     <section class="section-actions">
-      <router-link class="section-actions__link" to="/user/login"
-        >前台登入</router-link
-      >
+      <router-link class="section-actions__link" to="/user/login">
+        前台登入
+      </router-link>
     </section>
   </div>
 </template>
 
 <script>
-import authorizationAPI from './../../apis/authorization'
-import { Toast } from './../../utils/helpers'
+import LoginForm from '@/components/LoginForm'
 
 export default {
+  components: {
+    LoginForm,
+  },
   data() {
     return {
-      email: '',
-      password: '',
-      isProcessing: false,
+      title: '後台登入',
+      // 登入成功跳轉頁面
+      path: { name: 'AdminAllTweets' },
     }
-  },
-  methods: {
-    async handSubmit() {
-      try {
-        if (!this.email || !this.password) {
-          Toast.fire({
-            icon: 'warning',
-            title: '請填入 email 和 password',
-          })
-          return
-        }
-
-        this.isProcessing = true
-
-        const { data } = await authorizationAPI.signIn({
-          email: this.email,
-          password: this.password,
-        })
-        if (data.status !== 'success') {
-          throw new Error(data.message)
-        }
-
-        //將伺服器發送的 token 保存在 localStorage
-        localStorage.setItem('token', data.token)
-
-        // 將資料傳入Vuex中
-        this.$store.commit('setCurrentUser', data.user)
-
-        //authorization 登入成功轉去推文列表
-        this.$router.push('/admin/tweets')
-      } catch (err) {
-        this.isProcessing = false
-        this.password = ''
-        Toast.fire({
-          icon: 'warning',
-          title: '輸入的帳號密碼有誤',
-        })
-      }
-    },
   },
 }
 </script>
@@ -112,55 +46,6 @@ export default {
     color: var(--blue-light);
     font-size: 18px;
     font-weight: bold;
-  }
-}
-.form {
-  margin: 0 auto;
-  width: 540px;
-  max-width: 90vw;
-  &__title {
-    font-size: 23px;
-    font-weight: bold;
-    color: var(--text);
-  }
-  &__email,
-  &__password {
-    position: relative;
-    margin-top: 20px;
-    border-radius: 4px;
-    border-bottom: 2px solid var(--gray-500);
-    &__label {
-      position: absolute;
-      top: 5px;
-      left: 10px;
-      font-size: 15px;
-      font-weight: 500;
-      color: var(--gray-500);
-    }
-    &__input {
-      width: 100%;
-      height: 50px;
-      padding: 0 10px;
-      padding-top: 20px;
-      background-color: var(--input-bg);
-      border: none;
-      font-size: 19px;
-      font-weight: 500;
-      color: var(--black);
-    }
-  }
-  &__submit {
-    margin-top: 30px;
-    width: 100%;
-    height: 50px;
-    background-color: var(--theme);
-    border-radius: 50px;
-    font-size: 18px;
-    font-weight: bold;
-    color: var(--white);
-    &:disabled {
-      background-color: var(--theme-dark);
-    }
   }
 }
 </style>
