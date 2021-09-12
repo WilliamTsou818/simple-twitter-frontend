3a0d41858eebfd9991c296aff595c79b53581040
hugh <indexhui@gmail.com>
2021-09-13 02:54:37 +0800

    check adminAllUsers api

--- src/views/Admin/AdminAllUsers.vue
+++ src/views/Admin/AdminAllUsers.vue
@@ -2,10 +2,16 @@
   <div class="container container--admin">
     <Head :title="title" />
     <AdminUserCard />
+    <div v-for="user in users" :key="user.id">
+      {{ user.id }}
+    </div>
   </div>
 </template>
 
 <script>
+import adminAPI from '@/apis/admin'
+import { Toast } from './../../utils/helpers'
+
 import Head from '@/components/Head'
 import AdminUserCard from '@/components/AdminUserCard'
 
@@ -17,7 +23,27 @@ export default {
   data() {
     return {
       title: '使用者列表',
+      users: [],
     }
+  },
+  created() {
+    this.fetchUsers()
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const { data } = await adminAPI.users.get()
+        this.users = data
+        console.log(this.users)
+        this.isLoading = false
+      } catch (error) {
+        console.log('error', error)
+        Toast.fire({
+          icon: 'error',
+          title: '目前無法讀取所有使用者，請稍後再試',
+        })
+      }
+    },
   },
 }
 </script>
