6470ed7a1b8b085d52a4a8500407e9ccbacf9751
hugh <indexhui@gmail.com>
2021-09-15 01:06:05 +0800

    UserPopularTop UI

---
+++ src/components/UserPopularTop.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="popular">
+    <div class="popular__title">
+      Popular
+    </div>
+    <div class="popular__user-list">
+      <!-- <router-link :to="{ name: 'user-tweets', params: { id: user.id } }"> -->
+      <div class="popular__user-list__list">
+        <div class="popular__user-list__avatar"></div>
+        <div class="popular__user-list__info">
+          <div>user__name</div>
+          <div>user__account</div>
+        </div>
+        <button class="popular__user-list_action">
+          <div>追隨</div>
+          <div>正在跟隨</div>
+        </button>
+      </div>
+      <!-- </router-link> -->
+    </div>
+  </div>
+</template>
+
+<script>
+//TODO:如果是在 追蹤頁面要更改上層follow狀態
+export default {}
+</script>
+
+<style scoped lang="scss">
+.popular {
+  width: 350px;
+  background-color: var(--blue-gray-500);
+  margin-top: 15px;
+  margin-left: 30px;
+  margin-right: 82px;
+  border-radius: 14px;
+  min-height: 50px;
+  &__title {
+    font-size: 18px;
+    text-align: left;
+    padding: 10px 15px;
+    font-weight: bold;
+    color: var(--text);
+    border-bottom: 1px solid var(--blue-gray-600);
+  }
+  &__user-list {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid var(--blue-gray-600);
+    &__avatar {
+      width: 50px;
+      height: 50px;
+      background-color: #c4c4c4;
+      border-radius: 50px;
+    }
+    &__info {
+      flex: 1;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="home">
+    <NavBarAdmin class="sm-d-none" />
     <!-- TODO:開發中使用 -->
-    <router-link to="/">Home</router-link>
-    <router-link to="/user/login">前台登入</router-link>
-    <router-link to="/user/register">使用者註冊</router-link>
-    <router-link to="/admin/login">後台登入</router-link>
-    <router-link to="/admin/tweets">推文清單</router-link>
-    <router-link to="/admin/users">使用者列表</router-link>
-    <button @click.prevent.stop="handleLogout">登出(測試用)</button>
+    <div class="development-link">
+      <router-link to="/">Home</router-link>
+      <router-link to="/user/login">前台登入</router-link>
+      <router-link to="/user/register">使用者註冊</router-link>
+      <router-link to="/admin/login">後台登入</router-link>
+      <router-link to="/admin/tweets">推文清單</router-link>
+      <router-link to="/admin/users">使用者列表</router-link>
+      <button @click.prevent.stop="handleLogout">登出(測試用)</button>
+    </div>
+    <UserPopularTop />
   </div>
 </template>
 
 <script>
+import NavBarAdmin from './../components/NavBarAdmin.vue'
+import TabBarAdmin from './../components/TabBarAdmin.vue'
+import UserPopularTop from './../components/UserPopularTop.vue'
+
 export default {
   name: 'Home',
+  components: {
+    NavBarAdmin,
+    TabBarAdmin,
+    UserPopularTop,
+  },
   methods: {
     handleLogout() {
       console.log('登出(測試用)')
@@ -25,6 +38,15 @@ export default {
 
 <style scoped lang="scss">
 .home {
+  max-width: 1440px;
+  display: flex;
+  align-items: flex-start;
+  margin: 0 auto;
+  // TODO:測試用
+  border: 1px solid red;
+}
+.development-link {
+  flex: 1;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
@@ -34,12 +56,24 @@ export default {
     display: block;
   }
 }
+
 a,
 button {
   color: var(--text);
   margin: 20px;
   &:hover {
     color: var(--theme);
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .sm-d-none {
+    display: none;
+  }
+}
+@media screen and (min-width: 599px) {
+  .lg-d-none {
+    display: none;
   }
 }
 </style>
