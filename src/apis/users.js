e03a2ea4bc78bd2e0fab133417bc757c30883337
YPINPIN <yping611@gmail.com>
2021-09-14 16:39:35 +0800

    add signUp api and Toast tip

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,7 +1,20 @@
-import { apiHelper } from './../utils/helpers'
+import { apiHelper, apiHelper2 } from './../utils/helpers'
 
 export default {
+  // 取得目前使用者(順便驗證token)
   getCurrentUser() {
     return apiHelper.get('/users/current_user')
   },
+  // 使用者註冊
+  signUp({ account, name, email, password, checkPassword }) {
+    // FIXME: 等候端Deploy到heroku之後進行修改
+    return apiHelper2.post('/users', {
+      account,
+      name,
+      email,
+      password,
+      checkPassword,
+    })
+  },
 }
+
