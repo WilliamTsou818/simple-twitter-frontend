c1a31184bb4db16fe584ae6fd43849b9615b6229
hugh <indexhui@gmail.com>
2021-09-15 19:57:08 +0800

    head UI

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <div v-show="backArrow">
-      <img src="@/assets/images/icon/back.svg" alt="back" />
+  <div class="head">
+    <div class="head__back" v-show="backArrow">
+      <img
+        class="head__back__icon"
+        src="@/assets/images/icon/back.svg"
+        alt="back"
+      />
     </div>
-    <div class="container__head">{{ title }}</div>
+    <div class="head__info">
+      <div class="head__info__title">{{ title }}</div>
+      <div v-show="count" class="head__info__count">{{ count }} 推文</div>
+    </div>
   </div>
 </template>
 
@@ -13,21 +20,65 @@ export default {
   props: {
     backArrow: {
       type: String,
-      default: false,
+      default: true,
     },
     title: {
       type: String,
       required: true,
+    },
+    count: {
+      type: String,
+      default: '',
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.container__head {
+.head {
+  position: sticky;
+  top: 0;
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  height: 56px;
   text-align: left;
+  background-color: var(--white);
   border-bottom: 1px solid var(--gray-200);
   font-weight: bold;
-  padding: 1rem 1.5rem;
+  padding: 0 12px;
+  &__back {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 32px;
+    height: 32px;
+    margin-right: 32px;
+    border-radius: 16px;
+    cursor: pointer;
+    transition-duration: 0.2s;
+    transition-property: background-color;
+    &:hover {
+      background-color: var(--gray-100);
+    }
+    &__icon {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  &__info {
+    display: flex;
+    flex-direction: column;
+    &__title {
+      font-size: 18px;
+      font-weight: 900;
+    }
+    &__count {
+      font-size: 13px;
+      line-height: 13px;
+      color: var(--gray-500);
+      font-weight: 500;
+    }
+  }
 }
 </style>
