6d5647c06a38e267ec149b9fe3d828e8f3052fa2
YPINPIN <yping611@gmail.com>
2021-09-20 01:13:43 +0800

    add AccountForm input validation

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -8,13 +8,23 @@
         name="account"
         type="text"
         pattern="[a-zA-Z0-9_]+"
-        :maxlength="accountMaxLength"
         required
       />
-      <div class="form__account__border"></div>
+      <div
+        :class="['form__account__border', { error: accountTip.length > 0 }]"
+      ></div>
       <label for="form__account__input" class="form__account__label">
         帳號
       </label>
+      <span class="form__account__tip">{{ accountTip }}</span>
+      <span
+        :class="[
+          'form__account__length',
+          { error: accountLength > accountMaxLength },
+        ]"
+      >
+        {{ accountLength }} / {{ accountMaxLength }}
+      </span>
     </div>
     <div class="form__name">
       <input
@@ -23,13 +33,18 @@
         id="name"
         name="name"
         type="text"
-        :maxlength="nameMaxLength"
         required
       />
-      <div class="form__name__border"></div>
+      <div :class="['form__name__border', { error: nameTip.length > 0 }]"></div>
       <label for="form__name__input" class="form__name__label">
         名稱
       </label>
+      <span class="form__name__tip">{{ nameTip }}</span>
+      <span
+        :class="['form__name__length', { error: nameLength > nameMaxLength }]"
+      >
+        {{ nameLength }} / {{ nameMaxLength }}
+      </span>
     </div>
     <div class="form__email">
       <input
@@ -40,10 +55,13 @@
         type="email"
         required
       />
-      <div class="form__email__border"></div>
+      <div
+        :class="['form__email__border', { error: emailTip.length > 0 }]"
+      ></div>
       <label for="form__email__input" class="form__email__label">
         Email
       </label>
+      <span class="form__email__tip">{{ emailTip }}</span>
     </div>
     <div class="form__password">
       <input
@@ -52,14 +70,23 @@
         id="password"
         name="password"
         type="password"
-        :minlength="passwordMinLength"
-        :maxlength="passwordMaxLength"
         required
       />
-      <div class="form__password__border"></div>
+      <div
+        :class="['form__password__border', { error: passwordTip.length > 0 }]"
+      ></div>
       <label for="form__password__input" class="form__password__label">
         密碼
       </label>
+      <span class="form__password__tip">{{ passwordTip }}</span>
+      <span
+        :class="[
+          'form__password__length',
+          { error: passwordLength > passwordMaxLength },
+        ]"
+      >
+        {{ passwordLength }} / {{ passwordMaxLength }}
+      </span>
     </div>
     <div class="form__checkPassword">
       <input
@@ -68,17 +95,21 @@
         id="checkPassword"
         name="checkPassword"
         type="password"
-        :minlength="passwordMinLength"
-        :maxlength="passwordMaxLength"
         required
       />
-      <div class="form__checkPassword__border"></div>
+      <div
+        :class="[
+          'form__checkPassword__border',
+          { error: checkPasswordTip.length > 0 },
+        ]"
+      ></div>
       <label
         for="form__checkPassword__input"
         class="form__checkPassword__label"
       >
         密碼確認
       </label>
+      <span class="form__checkPassword__tip">{{ checkPasswordTip }}</span>
     </div>
     <template v-if="$route.name === 'UserRegister'">
       <button class="form__submit" type="submit" :disabled="isProcessing">
@@ -136,48 +167,97 @@ export default {
       passwordMaxLength: 50,
       // 註冊中
       isProcessing: false,
+      // 帳號格式
+      regex: /^[a-zA-Z0-9_-]+$/,
+      // 驗證提示訊息
+      accountTip: '',
+      nameTip: '',
+      emailTip: '',
+      passwordTip: '',
+      checkPasswordTip: '',
     }
+  },
+  computed: {
+    // 當前字數
+    accountLength() {
+      return this.account.length
+    },
+    nameLength() {
+      return this.name.length
+    },
+    passwordLength() {
+      return this.password.length
+    },
   },
   watch: {
     initIsProcessing() {
       this.isProcessing = this.initIsProcessing
     },
+    account() {
+      if (!this.account) {
+        this.accountTip = '請填寫帳號'
+      } else if (this.account.length > this.accountMaxLength) {
+        this.accountTip = `帳號上限 ${this.accountMaxLength} 字`
+      } else if (!this.regex.test(this.account)) {
+        this.accountTip = '帳號只能為英文、數字與_ '
+      } else {
+        this.accountTip = ''
+      }
+    },
+    name() {
+      if (!this.name) {
+        this.nameTip = '請填寫名稱'
+      } else if (this.name.length > this.nameMaxLength) {
+        this.nameTip = `名稱上限 ${this.nameMaxLength} 字`
+      } else {
+        this.nameTip = ''
+      }
+    },
+    email() {
+      if (!this.email) {
+        this.emailTip = '請填寫Email'
+      } else {
+        this.emailTip = ''
+      }
+    },
+    password() {
+      if (!this.password) {
+        this.passwordTip = '請填寫密碼'
+      } else if (this.password.length > this.passwordMaxLength) {
+        this.passwordTip = `密碼上限 ${this.passwordMaxLength} 字`
+      } else if (this.password.length < this.passwordMinLength) {
+        this.passwordTip = `密碼至少 ${this.passwordMinLength} 個字`
+      } else {
+        this.passwordTip = ''
+      }
+      if (this.checkPassword.length > 0) {
+        this.checkPasswordTip =
+          this.password !== this.checkPassword ? '密碼輸入不一致' : ''
+      }
+    },
+    checkPassword() {
+      if (!this.checkPassword) {
+        this.checkPasswordTip = '請填寫密碼確認'
+      } else if (this.password !== this.checkPassword) {
+        this.checkPasswordTip = '密碼輸入不一致'
+      } else {
+        this.checkPasswordTip = ''
+      }
+    },
   },
   methods: {
     // 表單驗證
     formValidation() {
-      let toastTip = ''
       if (
-        !this.account ||
-        !this.name ||
-        !this.email ||
-        !this.password ||
-        !this.checkPassword
+        this.accountTip.length > 0 ||
+        this.nameTip.length > 0 ||
+        this.emailTip.length > 0 ||
+        this.passwordTip.length > 0 ||
+        this.checkPasswordTip.length > 0
       ) {
-        toastTip = '請填寫所有資料'
-      } else if (this.account.length > this.accountMaxLength) {
-        this.account = ''
-        toastTip = `帳號上限 ${this.accountMaxLength} 字`
-      } else if (this.name.length > this.nameMaxLength) {
-        this.name = ''
-        toastTip = `名稱上限 ${this.nameMaxLength} 字`
-      } else if (this.password.length > this.passwordMaxLength) {
-        this.password = ''
-        this.checkPassword = ''
-        toastTip = `密碼上限 ${this.passwordMaxLength} 字`
-      } else if (this.password.length < this.passwordMinLength) {
-        this.password = ''
-        this.checkPassword = ''
-        toastTip = `密碼至少 ${this.passwordMinLength} 個字`
-      } else if (this.password !== this.checkPassword) {
-        this.checkPassword = ''
-        toastTip = `密碼輸入不一致`
-      }
-
-      if (toastTip.length !== 0) {
         Toast.fire({
           icon: 'warning',
-          title: toastTip,
+          title: '請正確填寫所有資料',
         })
         return false
       }
@@ -230,6 +310,10 @@ export default {
       font-weight: 500;
       color: var(--black);
     }
+    &__input:hover ~ &__border:not(.error),
+    &__input:focus ~ &__border:not(.error) {
+      border-bottom: 2px solid var(--input-hover-border);
+    }
     &__border {
       position: absolute;
       top: 50px;
@@ -238,10 +322,28 @@ export default {
       height: 2px;
       border-radius: 0 0 4px 4px;
       background-color: var(--gray-500);
+      &.error {
+        border-bottom: 2px solid var(--input-error-border);
+      }
     }
-    &__input:hover ~ &__border,
-    &__input:focus ~ &__border {
-      border-bottom: 2px solid var(--input-hover-border);
+    &__tip {
+      position: absolute;
+      top: 52px;
+      left: 0;
+      font-size: 15px;
+      font-weight: 500;
+      color: var(--input-error-border);
+    }
+    &__length {
+      position: absolute;
+      top: 52px;
+      right: 0;
+      font-size: 15px;
+      font-weight: 500;
+      color: var(--gray-500);
+      &.error {
+        color: var(--input-error-border);
+      }
     }
   }
   &__submit {
