56cd91ed35c48398888698377e52de77e6e68b35
YPINPIN <yping611@gmail.com>
2021-09-24 18:30:18 +0800

    add socket connect

--- src/main.js
+++ src/main.js
@@ -2,9 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from 'socket.io-client'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import '@/assets/styles/main.scss'
+
+const socketOptions = {
+  auth: {
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUsImlhdCI6MTYzMjQ0Nzk1MX0.UR2Sey3xFt_PxaUaHswqhPboH8QSlhi62qAUN_7fDOM",
+  }
+}
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('https://wahp-simeple-twitter-api.herokuapp.com/', socketOptions),
+  vuex: {
+    store,
+    actionPrefix: "SOCKET_",
+  }
+})
+);
 
 const options = {
   // You can set your default options here
@@ -32,5 +50,17 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
+  sockets: {
+    connect() {
+      console.log('socket connected')
+    },
+    connect_error() {
+      console.log('socket connect_error')
+      this.$socket.close();
+    },
+    disconnect(reason) {
+      console.log('socket disconnect', reason)
+    },
+  },
 }).$mount('#app')
 
