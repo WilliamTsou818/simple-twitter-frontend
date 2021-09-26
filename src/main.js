7de9c935bf86d1545affb61b82dd68059efae268
YPINPIN <yping611@gmail.com>
2021-09-26 19:06:22 +0800

    remove useless code

--- src/main.js
+++ src/main.js
@@ -65,11 +65,6 @@ new Vue({
     error(error) {
       console.log('socket error', error)
     },
-    // TODO:這邊之後如果沒有需要就可以刪掉
-    // 私人聊天室(訊息通知)
-    privateMessage(data) {
-      console.log('privateMessage back', data)
-    },
   },
 }).$mount('#app')
 
